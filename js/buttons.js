8b91ce1b2eef10be6b0d27e302e8b070314d622e
Marimar8888 <alonsomarimar@hotmail.com>
2023-11-17 12:49:17 +0100

    links

---
+++ js/buttons.js
@@ -0,0 +1,9 @@
+document.getElementById("btn-login").addEventListener("click", function() {
+ 
+    window.location.href = "https://codepen.io/login";
+});
+
+
+document.getElementById("btn-signup").addEventListener("click", function() {
+    window.location.href = "https://codepen.io/accounts/signup/user/free";
+});
